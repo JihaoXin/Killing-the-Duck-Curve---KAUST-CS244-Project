87cbc8e265002d20f44daaf9024be49d3c6df504
KOISHI-CHEN <75569629+KOISHI-CHEN@users.noreply.github.com>
2022-12-02 02:59:21 +0300

    test

--- out/clang-release/greenwireless.o.d
+++
@@ -1,6 +0,0 @@
-out/clang-release//greenwireless.o: greenwireless.cpp \
-  C:/omnetpp-6.0.1/include/omnetpp.h greenwireless_m.h
-
-C:/omnetpp-6.0.1/include/omnetpp.h:
-
-greenwireless_m.h:
--- out/clang-release/greenwireless_m.o.d
+++
@@ -1,6 +0,0 @@
-out/clang-release//greenwireless_m.o: greenwireless_m.cpp \
-  greenwireless_m.h C:/omnetpp-6.0.1/include/omnetpp.h
-
-greenwireless_m.h:
-
-C:/omnetpp-6.0.1/include/omnetpp.h:
